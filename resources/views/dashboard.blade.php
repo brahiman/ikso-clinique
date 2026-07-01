7cb6224b5541bc0de5361ea837a49ec3f69a0212
moussa <107997938+YonliMoussa@users.noreply.github.com>
2026-07-01 19:25:05 +0000

    ok

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,8 +5,20 @@
 @section('content')
     <div class="row">
         <div class="col-12">
-            <h4 class="mb-sm-0">Bienvenue sur le Tableau de Bord de la Clinique</h4>
+            <h4 class="mb-sm-0 ">Bienvenue sur le Tableau de Bord de la Clinique</h4>
         </div>
+        <div class="card col-lg-3 m-3 col-sm-6">
+            <div class="card-body">
+                <h3 class="accordion">Affichage dynamique en fonction des roles</h3>
+            </div>
+
+        </div>
+    </div>
+    <div class="row">
+
+        <div class="col-6">1</div>
+        <div class="col-3">2</div>
+
     </div>
 
     <!-- Ici tu pourras mettre les stats : Nombre de patients, RDV aujourd'hui, etc. -->
