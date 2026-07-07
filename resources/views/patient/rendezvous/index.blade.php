f1eb528d7d9bb5476f49f2a706595fdcaf0b7572
Brahiman BOURGOU <brahimabourg@gmail.com>
2026-07-07 13:50:18 +0000

    gestion compte patient:rdv suite

--- resources/views/patient/rendezvous/index.blade.php
+++ resources/views/patient/rendezvous/index.blade.php
@@ -3,13 +3,13 @@
 @section('content')
     <div class="page-content">
         <div class="container-fluid">
-            <h4 class="mb-3">Mes Rendez-vous (pour moi et mes proches)</h4>
+            <h4 class="mb-4">Mes Rendez-vous (pour moi et mes proches)</h4>
 
             <div class="table-responsive">
-                <table class="table table-bordered">
+                <table class="table table-bordered table-hover">
                     <thead>
                     <tr>
-                        <th>Date & Heure</th>
+                        <th style="width: 28%; background-color: #f8f9fa;">Date & Heure</th>
                         <th>Patient</th>
                         <th>Médecin</th>
                         <th>Statut</th>
@@ -18,11 +18,16 @@
                     <tbody>
                     @foreach($rendezVous as $rdv)
                         <tr>
-                            <td>{{ $rdv->date_heure->format('d/m/Y H:i') }}</td>
-                            <td>{{ $rdv->patient->prenom }} {{ $rdv->patient->nom }}</td>
+                            <td style="font-size: 1.1rem; font-weight: bold; color: #0d6efd;">
+                                {{ $rdv->date_heure->format('d/m/Y') }}<br>
+                                <small style="color: #6c757d;">{{ $rdv->date_heure->format('H:i') }}</small>
+                            </td>
+                            <td>
+                                <strong>{{ $rdv->patient->prenom }} {{ $rdv->patient->nom }}</strong>
+                            </td>
                             <td>Dr. {{ $rdv->medecin->user->name }}</td>
                             <td>
-                            <span class="badge bg-{{ $rdv->statut == 'planifie' ? 'warning' : 'success' }}">
+                            <span class="badge bg-{{ $rdv->statut == 'planifie' ? 'warning' : 'success' }} fs-6">
                                 {{ ucfirst($rdv->statut) }}
                             </span>
                             </td>
